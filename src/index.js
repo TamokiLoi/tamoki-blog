1d060847e347ea6e819d8561f3360bc8267d9f79
loinlt <loinguyenlamthanh@gmail.com>
2021-06-22 18:31:40 +0700

    update src

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 const express = require('express');
-const morgan  = require('morgan');
-const expressHandlebars  = require('express-handlebars');
+const morgan = require('morgan');
+const expressHandlebars = require('express-handlebars');
 const path = require('path');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({ extended: true }));
+
+// XMLHttpRequest, fetch, axios, ajax
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('.hbs', expressHandlebars({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-app.get('/news', (req, res) => {
-    res.render('news');
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-})
+});
